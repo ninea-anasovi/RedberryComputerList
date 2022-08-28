5ad38ccdcfcc8bb8edd364652af4f662a7b22a87
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-08-28 14:12:32 +0400

    landing page creation

---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Logo from '../../../assets/images/LOGO.svg'
+import HeroImage from '../../../assets/images/hero-image.png'
+import { Link } from "react-router-dom"
+
+function Home() {
+  return (
+    <div className='my-16'>
+        <div>
+            <img src={Logo} alt='redberry-logo' className='ml-auto mr-auto'/>
+        </div>
+        <div className='my-16'>
+            <img src={HeroImage} alt='man working in redberry' className='ml-auto mr-auto w-2/5'/>
+        </div>
+        <div className=''>
+            <Link to="/form">
+                <button className='block bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 w-1/4 text-center rounded-md ml-auto mr-auto'>
+                    ᲩᲐᲜᲐᲬᲔᲠᲘᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ
+                </button>
+            </Link>
+            <Link to="/list">
+                <button href='#' className='block bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 w-1/4 text-center rounded-md ml-auto mr-auto mt-4'>
+                    ᲩᲐᲜᲐᲬᲔᲠᲔᲑᲘᲡ ᲡᲘᲐ
+                </button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
