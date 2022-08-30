e0eca615b67b81482b79ca41b0fd537b77f5d2d9
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-08-30 13:40:08 +0400

    remove focus outlines

--- src/components/pages/wizardFrom/FormDropdown.jsx
+++ src/components/pages/wizardFrom/FormDropdown.jsx
@@ -5,7 +5,7 @@ function FormDropdown({ fieldName, label, options }) {
 
   return (
     <div>
-        <Field as="select" name={fieldName} className="bg-[#F7F7F7] border-2 border-[#F7F7F7] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-9">
+        <Field as="select" name={fieldName} className="bg-[#F7F7F7] border-2 border-[#F7F7F7] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-9 focus:outline-0 focus:border-[#F7F7F7]">
             <option value="" className='my-2'>{label}</option>
             {
               options.map((item) => (
