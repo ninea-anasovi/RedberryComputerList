e4698cefea9aaac71ad99cdae30762fa97500a3d
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-09-05 21:25:26 +0400

    fix: dropdown error change

--- src/components/pages/wizardFrom/FormDropdown.jsx
+++ src/components/pages/wizardFrom/FormDropdown.jsx
@@ -10,7 +10,7 @@ function FormDropdown(props) {
     <div>
         <select name={name} {...field} 
           className={"bg-[#F7F7F7] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-9 focus:outline-0 focus:border-[#F7F7F7] " + dropdownStyle}
-          handleChange={(event: FormEvent) => {name === 'team_id' &&  setTeamID(event.target.value)}} 
+          handleChange={(event) => {name === 'team_id' &&  setTeamID(event.target.value)}} 
         >
             <option value="" className='my-2'>{label}</option>
             {
